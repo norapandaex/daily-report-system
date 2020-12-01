60d8050bb54463894784bb364b082dfda7715b6a
hiro <good.boys@live.jp>
2020-12-01 10:41:35 +0900

    Add Employee create

--- src/utils/EncryptUtil.java
+++ src/utils/EncryptUtil.java
@@ -6,6 +6,8 @@ import java.security.NoSuchAlgorithmException;
 import javax.xml.bind.DatatypeConverter;
 
 public class EncryptUtil {
+    //getPasswordEncrypt メソッドは、引数で受け取った文字列にペッパー文字列を連結させたものを SHA256 でハッシュ化します。
+    //引数の文字列が何もなければ、空の文字列を返します。
     public static String getPasswordEncrypt(String plain_p, String pepper) {
         String ret = "";
 
