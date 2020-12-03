a0cc31dc6d03a8870a97b2d1bd2bcc7469367160
hiro <good.boys@live.jp>
2020-12-03 13:22:14 +0900

    Add Report show

--- src/controllers/reports/ReportCreateServlet.java
+++ src/controllers/reports/ReportCreateServlet.java
@@ -47,7 +47,7 @@ public class ReportCreateServlet extends HttpServlet {
 
             Date report_date = new Date(System.currentTimeMillis());
             String rd_str = request.getParameter("report_date");
-            if(rd_str != null && !rd_str.equals("")) {
+            if(rd_str != null && !rd_str.equals("")) { //日付欄をわざと未入力にした場合、当日の日付を入れるようにしている
                 report_date = Date.valueOf(request.getParameter("report_date"));
             }
             r.setReport_date(report_date);
